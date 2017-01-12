916ca1a3a870397b63b924437db8f678e1a0bbb0
Rafa Rodriguez <leafarx@gmail.com>
2017-01-12 03:21:14 -0600

    probando click()

--- js/javascript3.js
+++ js/javascript3.js
@@ -1,6 +1,5 @@
 // JavaScript Document
 
-
 function loadJSON(callback) {   
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("web/json");
@@ -18,18 +17,15 @@ function loadJSON(callback) {
 function leer_jason() {
 	loadJSON(function(response) {
  		var actual_JSON = JSON.parse(response);
- 		Object.keys(actual_JSON.MATERIAS).forEach(function(key) {		//actual_JSON.MATERIAS[0].UNIDAD
+ 		Object.keys(actual_JSON.MATERIAS).forEach(function(key) {
 			add_menu_materias(actual_JSON.MATERIAS[key].name);
-			//document.getElementById("texto").innerHTML = actual_JSON.MATERIAS[key].name;
 		});
 		
 		Object.keys(actual_JSON.MATERIAS[0].UNIDAD).forEach(function(key) {
-			//document.getElementById("texto").innerHTML = actual_JSON.MATERIAS[0].UNIDAD[key].name;
 			add_menu_unidades(actual_JSON.MATERIAS[0].UNIDAD[key].id, actual_JSON.MATERIAS[0].UNIDAD[key].name);
 		});
 		
 		Object.keys(actual_JSON.MATERIAS[0].UNIDAD[0].TEMA).forEach(function(key) {
-			//document.getElementById("texto").innerHTML = actual_JSON.MATERIAS[0].UNIDAD[0].TEMA[key].name;
 			add_menu_tema(actual_JSON.MATERIAS[0].UNIDAD[0].TEMA[key].id, actual_JSON.MATERIAS[0].UNIDAD[0].TEMA[key].name, actual_JSON.MATERIAS[0].UNIDAD[0].TEMA[key].sub_name);
 		});
 		
@@ -40,9 +36,17 @@ function add_menu_materias(name) {
 	var ul = document.getElementById("menu_materias");
 	var li = document.createElement('li');
 	var h4 = document.createElement('h4');
+	var a = document.createElement('a');
+	a.setAttribute('href', '#');
 	h4.appendChild(document.createTextNode(name));
 	li.appendChild(h4);
-	ul.appendChild(li);
+	a.appendChild(li);
+	ul.appendChild(a);
+	
+	
+	$("a li").click(function(){
+        document.getElementById("texto").innerHTML = $(this).text();
+    });
 }
 
 function add_menu_unidades(id, name) {
@@ -50,11 +54,17 @@ function add_menu_unidades(id, name) {
 	var li = document.createElement('li');
 	var h4 = document.createElement('h4');
 	var strong = document.createElement('strong');
+	var a = document.createElement('a');
+	a.setAttribute('href', '#');
 	strong.appendChild(document.createTextNode(id));
 	h4.appendChild(strong);
 	h4.appendChild(document.createTextNode("  "+name));
 	li.appendChild(h4);
-	ul.appendChild(li);
+	// if tiene contenido
+	a.appendChild(li);
+	ul.appendChild(a);
+	//else
+	//	ul.appendChild(li);
 }
 
 function add_menu_tema(id, name, sub_name) {
@@ -64,6 +74,8 @@ function add_menu_tema(id, name, sub_name) {
 	var strong = document.createElement('strong');
 	var h6 = document.createElement('h6');
 	var em = document.createElement('em');
+	var a = document.createElement('a');
+	a.setAttribute('href', '#');
 	strong.appendChild(document.createTextNode(id));
 	h4.appendChild(strong);
 	h4.appendChild(document.createTextNode("  "+name));
@@ -72,5 +84,13 @@ function add_menu_tema(id, name, sub_name) {
 	h6.appendChild(em);
 	li.appendChild(h4);
 	li.appendChild(h6);
-	ul.appendChild(li);
+	// if tiene contenido
+	a.appendChild(li);
+	ul.appendChild(a);
+	//else
+	//	ul.appendChild(li);
+}
+
+function hola2(opc){
+	document.getElementById("texto").innerHTML = "Saz "+opc + " x";
 }
