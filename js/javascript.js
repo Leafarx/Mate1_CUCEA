a81075c7f6be08ae9c2e32311fc9718fb4abcf70
Rafa Rodriguez <leafarx@gmail.com>
2017-01-22 21:47:09 -0600

    limpia hecha

--- js/javascript.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-
-function cambiar_menu(id_ocultar, id_mostrar) {
- // document.getElementById("mensaje").innerHTML = id_ocultar + "->" + id_mostrar ;
-  document.getElementById(id_ocultar).style.display = 'none';
-  document.getElementById(id_mostrar).style.display = 'inherit';
-}
